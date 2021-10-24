c57005d368af744218cb606acda281fa36961691
Jessen Gilbert <jessen.gilbert@yahoo.com>
2021-10-24 17:23:00 -0600

    fix improper types and add name prop

---
+++ BudgetWise.Api/Models/Requests/LabelCreateRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace BudgetWise.Api.Models.Requests
+{
+    public class LabelCreateRequest
+    {
+        [JsonPropertyName(("name"))]
+        public string Name { get; set; }
+        
+        [JsonPropertyName("category_id")]
+        public int CategoryId { get; set; }
+        
+        [JsonPropertyName("planned_amount")]
+        public decimal PlannedAmount { get; set; }
+        
+        [JsonPropertyName("applied_amount")]
+        public int AppliedAmount { get; set; }
+        
+        [JsonPropertyName("notes")]
+        public string Notes { get; set; }
+        
+        [JsonPropertyName("due_date")]
+        public DateTime DueDate { get; set; }
+        
+    }
+}
